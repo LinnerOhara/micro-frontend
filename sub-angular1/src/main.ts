cadca7212a573432d6ae23fea18fa3530859ac52
柯培杰 <984728868@qq.com>
2023-08-16 10:32:25 +0800

    init sub-angular1 sub-vue2

---
+++ sub-angular1/src/main.ts
@@ -0,0 +1,33 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModuleRef } from '@angular/core';
+import { AppModule } from './app/app.module';
+import './public-path';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+let app: void | NgModuleRef<AppModule>;
+async function render() {
+  app = await platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+}
+
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap(props: Object) {
+  console.log('sub-angular bootstrap', props);
+}
+
+export async function mount(props: Object) {
+  console.log('sub-angular mount', props)
+  render();
+}
+
+export async function unmount(props: Object) {
+  console.log('sub-angular unmount', props);
+  // @ts-ignore
+  app.destroy();
+}
