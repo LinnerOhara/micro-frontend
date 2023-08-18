6b3dcc5c4bb7282af79f52cf4f9741c8775ba8eb
柯培杰 <984728868@qq.com>
2023-08-18 21:17:37 +0800

    新增tab标签页变更显示

--- micro-base/src/app.tsx
+++ micro-base/src/app.tsx
@@ -1,10 +1,39 @@
 import './app.css'
 import { Layout } from 'antd'
 import Header from './layout/Header';
+import { useEffect, useSyncExternalStore } from 'preact/compat';
 
 const { Content } = Layout;
 
+const tabVisibleSubscribe = (callback: () => void) => {
+  window.addEventListener('visibilitychange', callback)
+  return () => {
+    window.removeEventListener('visibilitychange', callback)
+  }
+}
+
 export function App() {
+  const visibleState = useSyncExternalStore(
+    tabVisibleSubscribe,
+    () => {
+      return document.visibilityState
+    } 
+  )
+  let timer: number
+
+  useEffect(() => {
+    if (visibleState === 'visible') {
+      document.title = '被你找到了'
+      timer = setTimeout(() => {
+        document.title = 'micro-frontend'
+      }, 3000)
+    } else if (visibleState === 'hidden') {
+      if (timer) {
+        clearTimeout(timer)
+      }
+      document.title = '你找不到我~~'
+    }
+  }, [visibleState])
 
   return (
     <Layout
