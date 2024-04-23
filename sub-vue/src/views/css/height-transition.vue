f25eb204d6c9ad33fbd31a01f13cc2ce5e37b2b2
柯培杰 <984728868@qq.com>
2024-04-24 01:34:47 +0800

    完善高度过度demo

--- sub-vue/src/views/css/height-transition.vue
+++ sub-vue/src/views/css/height-transition.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
+const btnRef = ref<HTMLDivElement>()
+const detailRef = ref<HTMLDivElement>()
+
+const btnMouseEnter = () => {
+  // 造成回流，但没绘制
+  detailRef.value!.style.height = 'auto'
+  const { height } = detailRef.value!.getBoundingClientRect();
+  detailRef.value!.style.height = '0'
+  detailRef.value!.style.transition = 'height 0.5s'
+  // detailRef.value?.offsetHeight; // 强制回流
+  // 或者使用 requestAnimationFrame 让它在下一步绘制
+  console.log(detailRef.value)
+  requestAnimationFrame(() => {
+    detailRef.value!.style.height = height + 'px';
+  })
+}
+
+const btnMouseLeave = () => {
+  detailRef.value!.style.height = '0'
+}
 
 </script>
 
 <template>
   <div class="container">
-    <div class="btn">
+    <div class="btn" ref="btnRef" @mouseenter="btnMouseEnter" @mouseleave="btnMouseLeave">
       <span>hover me</span>
-      <div class="detail">
+      <div class="detail" ref="detailRef">
         <div class="content">
           <div class="inner">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut autem, cum debitis delectus expedita facere fuga itaque laboriosam laudantium praesentium reiciendis repudiandae temporibus unde veniam. Aperiam dolorem magni ut.
@@ -36,8 +58,14 @@
     background: cornflowerblue;
 
     .detail {
-      height: auto;
-      padding: 20px;
+      //height: auto;
+      //padding: 20px;
+      // 通过高度实现
+      //max-height: 99999999999999px;
+      // 通过缩放实现
+      //transform: scale(1);
+      // 通过网格布局实现
+      //grid-template-rows: 1fr;
     }
   }
 
@@ -52,6 +80,18 @@
     height: 0;
     padding: 0;
     overflow: hidden;
+
+    .inner {
+      margin: 20px;
+    }
+    //transition: 0.5s;
+    // 通过高度实现
+    //max-height: 0;
+    // 通过缩放实现
+    //transform: scale(0);
+    // 通过网格布局实现
+    //display: grid;
+    //grid-template-rows: 0fr;
   }
 }
 </style>
